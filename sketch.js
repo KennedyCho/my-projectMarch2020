ac89b35c2989daf9b00cbab0d3defa935a3061ac
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2020-03-09 18:23:16 -1000

    rotate around point exercise

--- sketch.js
+++ sketch.js
@@ -1,9 +1,18 @@
-function setup(){
-
-
+function setup() {
+  createCanvas(400, 400);
+  angleMode(DEGREES);
 }
-
-function draw(){
-
-  
+ 
+function draw() {
+  background(220);
+ 
+  text("horizontal", 10, 30);
+  rotateAbout(10, 30, 45)
+  text("vertical", 10, 30);
+}
+ 
+function rotateAbout(x, y, angle) {
+  translate(x, y);
+  rotate(angle);
+  translate(-x, -y);
 }
