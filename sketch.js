be3a03b0734b713155a1463768b9089894c7a855
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2020-03-16 16:39:47 -1000

    fft analysis

--- sketch.js
+++ sketch.js
@@ -1,16 +1,45 @@
 var micInput;
+var sampleAudio;
+let amp;
+
+function preload(){
+
+  // mp3 file
+  sampleAudio = loadSound('testAudio.mp3')
+
+}
 
 function setup() {
   createCanvas(400, 400);
 
+  // play mp3 file
+  // sampleAudio.play();
+
   //audio input using mic
   micInput = new p5.AudioIn();
   micInput.start();
+
+  // amplitude object
+  amp = new p5.Amplitude();
+  // fft object
+  fft = new p5.FFT();
+
+  fft.setInput(micInput);
+
 }
 
 function draw() {
+  // assign fft analysis
+  let spectrum = fft.analyze();
 
   // Get the overall volume (between 0 and 1.0)
-  let vol = micInput.getLevel();
-  console.log(vol);
+  let micVol = micInput.getLevel();
+
+  let ampTest = amp.getLevel();
+
+  // test output
+  console.log("Mic Amp: "+micVol);
+
+
+
 }
