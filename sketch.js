32b6dbb930d9426bc0eb1ae9a8e363a38da79845
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2020-03-21 12:21:33 -1000

    3d shape audio control test

--- sketch.js
+++ sketch.js
@@ -8,7 +8,19 @@ var toggleButton;
 
 var micInput;
 var sampleAudio;
-let amp;
+
+// freq level
+let bassLevel;
+let lowMidLevel;
+let midLevel;
+let highMidLevel;
+let trebleLevel;
+
+// rotation
+let angleX = 0;
+let angleY = 0;
+let angleZ = 0;
+
 
 function preload(){
 
@@ -17,64 +29,62 @@ function preload(){
 
 }
 
-function mousePressed() {
-  if (sampleAudio.isPlaying()) {
-    sampleAudio.pause();
-    // background("grey");
-  }else {
-    sampleAudio.play();
-    // background("pink");
-  }
-}
-
 function setup() {
-  createCanvas(400, 400);
-  background("grey");
+  createCanvas(400, 400, WEBGL);
+  background(175);
+
+  // colorMode(HSB);
 
   // play mp3 file
   // sampleAudio.play();
 
   //audio input using mic
-  // micInput = new p5.AudioIn();
-  // micInput.start();
+  micInput = new p5.AudioIn();
+  micInput.start();
 
   // fft object
   fft = new p5.FFT(0, 256); //bin must be power of 2
 
   // analyze microphone input
-  // fft.setInput(micInput);
+  fft.setInput(micInput);
 
   // analyze test audio file
-  fft.setInput(sampleAudio);
+  // fft.setInput(sampleAudio);
+
 }
 
+// function mousePressed() {
+//   if (sampleAudio.isPlaying()) {
+//     sampleAudio.pause();
+//   }else {
+//     sampleAudio.play();
+//   }
+// }
+
 function draw() {
-  background('grey');
+  background(175);
   // assign fft analysis
   let spectrum = fft.analyze(); // default 1024
-  console.log(spectrum);
+
   // Get the overall volume (between 0 and 1.0)
   // let micVol = micInput.getLevel();
-  stroke(255);
-  for (var i = 0; i < spectrum.length; i++) {
-    var amp = spectrum[i];
-    var y = map(amp, 0, 255, height, 0);
-    line(i*width/64, height, i*width/64, y);
-  }
 
+  bassLevel = fft.getEnergy("bass");
+  lowMidLevel = fft.getEnergy("lowMid");
+  midLevel = fft.getEnergy("mid");
+  highMidLevel = fft.getEnergy("highMid");
+  trebleLevel = fft.getEnergy("treble");
+  angleMode(DEGREES)
 
-  // "bass", "lowMid", "mid", "highMid", "treble"
-  let bassLevel = fft.getEnergy("bass");
-  let lowMidLevel = fft.getEnergy("lowMid");
-  let midLevel = fft.getEnergy("mid");
-  let highMidLevel = fft.getEnergy("highMid");
-  let trebleLevel = fft.getEnergy("treble");
+  rectMode(CENTER);
+  noStroke();
+  normalMaterial();
+  rotateY(angleY);
+  rotateX(angleX);
+  rotateZ(angleZ);
+  box(50, 50, 50);
 
-  // test output
-  // console.log("Mic Volume: "+micVol);
-  // console.log("FFT: "+spectrum[0]);
-  // console.log("Bass: "+bassVol);
-
-
-
+  angleY += map(bassLevel, 0, 255, 0, 4)
+  angleX += map(midLevel, 0, 255, 0, 4)
+  angleZ += map(trebleLevel, 0, 255, 0, 4)
 }
